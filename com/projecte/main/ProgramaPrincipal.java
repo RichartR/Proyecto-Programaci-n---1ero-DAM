4c515ee66738355c2e54ec21e395d6f7b7fa2a10
Lidtol <espinarlidia@gmail.com>
2025-05-16 18:57:09 +0200

    Paquete Lidia.

--- com/projecte/main/ProgramaPrincipal.java
+++ com/projecte/main/ProgramaPrincipal.java
@@ -1,5 +1,9 @@
 package com.projecte.main;
 
+import com.projecte.Lidia.Lidia;
+
 public class ProgramaPrincipal {
-    
+    public static void main(String[] args) {
+        Lidia.lidiaNombre(); // Llamada estática al método
+    }
 }

9b35a7f412a3db010ac3e1fbfc60c278f01ae1e5
NeimCode <CutyShyHealt@gmail.com>
2025-05-23 18:48:11 +0200

    Getters, Setters y Constructors de cada objeto.

--- com/projecte/Objetos/Director.java
+++ com/projecte/Objetos/Director.java
@@ -1,9 +1,48 @@
 package com.projecte.Objetos;
 
 public class Director {
+
     private String nombre;
     private int edad;
     private String nacionalidad;
     private String genero;
+
     
+
+        //Constructors
+
+        public Director(String nombre, int edad, String nacionalidad, String genero) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.nacionalidad = nacionalidad;
+            this.genero = genero;
+        }
+
+    //Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
 }
--- com/projecte/Objetos/Pelicula.java
+++ com/projecte/Objetos/Pelicula.java
@@ -5,4 +5,42 @@ public class Pelicula {
     private int anyoSalida; 
     private String genero;
     private String director;
+    
+
+
+    //Constructors
+    public Pelicula(String titulo, int anyoSalida, String genero, String director) {
+        this.titulo = titulo;
+        this.anyoSalida = anyoSalida;
+        this.genero = genero;
+        this.director = director;
+    }
+    //Getters y Setters
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAnyoSalida() {
+        return anyoSalida;
+    }
+    public void setAnyoSalida(int anyoSalida) {
+        this.anyoSalida = anyoSalida;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
+
+
